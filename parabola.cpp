76969670fe00a1e58165366bb7a65cd360aacd1e
nags5454 <35479075+nags5454@users.noreply.github.com>
2018-01-16 10:39:55 +0530

    Add files via upload

---
+++ parabola.cpp
@@ -0,0 +1,17 @@
+#include<iostraem>
+using namespace std;
+typedef struct
+{
+	int a,b,c;
+}param;
+void read_parameter(param *p)
+{
+	cout<<"Enter the value of the a,b,c"<<endl;
+	cin>>p->a>>p->b>>p->c;
+}
+void area(param p,int x,int y)
+{
+	float area,ll,ul
+	
+	for()
+}
