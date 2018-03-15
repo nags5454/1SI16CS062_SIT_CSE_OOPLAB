d3a93a4bb2c5a5ab3a682a5910376603a2e4c623
Nagabhushan N <35479075+nags5454@users.noreply.github.com>
2018-03-15 19:46:56 +0530

    Add files via upload

---
+++ fraction/main.cpp
@@ -0,0 +1,159 @@
+	#include <iostream>
+	using namespace std;
+	class FractionType
+	{
+		private:
+		    int iNum, iDen;
+
+		public:
+			FractionType(){}
+		    FractionType(int,int);
+
+		    void fnSetFraction(int, int);
+		    void fnShowFraction();
+
+			FractionType operator+(FractionType);//f1+f2 --> f1.operator+(f2)
+			FractionType operator+(int);			//f1.operator(int)
+			FractionType operator-(FractionType);
+			FractionType operator*(FractionType);
+			bool operator==(FractionType);
+			FractionType operator=(FractionType);
+
+
+			FractionType operator-(void);	// -f2	--> f2.operator-()
+			FractionType operator++(void);	// ++f3 --> f3.operator++()
+			FractionType operator--(void);
+			FractionType operator++(int);		//	postfix version
+	};
+
+	FractionType FractionType :: operator-(void)
+	{
+		FractionType temp;
+		temp.iNum = -iNum;
+		temp.iDen = iDen;
+		return temp;
+	}
+	// p = q			p.operator=(q)
+	FractionType FractionType :: operator=(FractionType f2)
+	{
+		iNum = f2.iNum;
+		iDen = f2.iDen;
+		return *this;
+	}
+
+
+	FractionType FractionType :: operator++(void)
+	{
+		cout << "\nPrefix version" << endl;
+		iNum = iNum + iDen;
+		return *this;
+	}
+
+	FractionType FractionType :: operator++(int dummy)
+	{
+		cout << "\nPostfix version" << endl;
+		FractionType old(*this);
+		iNum = iNum + iDen;
+		return old;
+	}
+
+	FractionType FractionType :: operator--(void)
+	{
+		iNum = iNum - iDen;
+		return *this;
+	}
+
+	FractionType FractionType :: operator+(int val)	//f3+5	f3.operator+(5)
+	{
+		FractionType f3;
+		f3.iNum = iNum + iDen * val;
+		f3.iDen = iDen;
+		return f3;
+	}
+
+
+	bool FractionType :: operator==(FractionType f2)
+	{
+		if(iNum * f2.iDen == iDen * f2.iNum)
+			return true;
+		else
+			return false;
+	}
+
+	FractionType FractionType :: operator*(FractionType f2)
+	{
+		FractionType f3;
+		f3.iNum = iNum * f2.iNum;
+		f3.iDen = iDen * f2.iDen;
+		return f3;
+	}
+
+
+	FractionType FractionType :: operator-(FractionType f2)
+	{
+		FractionType f3;
+		f3.iNum = iNum * f2.iDen - iDen * f2.iNum;
+		f3.iDen = iDen * f2.iDen;
+		return f3;
+	}
+
+
+	FractionType FractionType :: operator+(FractionType f2)
+	{
+		FractionType f3;
+		f3.iNum = iNum * f2.iDen + iDen * f2.iNum;
+		f3.iDen = iDen * f2.iDen;
+		return f3;
+	}
+
+
+	FractionType :: FractionType(int iVal1, int iVal2)
+	{
+	//    cout << "\nTwo parameter constructor\n";
+		iNum = iVal1;
+		iDen = iVal2;
+	}
+
+	void FractionType :: fnSetFraction(int iN, int iD)
+	{
+		iNum = iN;
+		iDen = iD;
+	}
+
+	void FractionType :: fnShowFraction()
+	{
+		cout << "Fraction : " << "( " << iNum << " / " << iDen << " )" << endl;
+	}
+
+
+	int main()
+	{
+		int a = 2, b = 7, c;
+
+		FractionType p(3,4), q(1,3);
+		FractionType r;
+
+
+		++a;
+		a++;
+		cout << "****CASE 1****\n";
+		cout << "p : ";
+		p.fnShowFraction();
+		q = ++p;
+		cout << "q : ";
+		q.fnShowFraction();
+		cout << "p : ";
+		p.fnShowFraction();
+
+		cout << "****CASE 2****\n";
+		p.fnSetFraction(3,4);
+		cout << "p : ";
+		p.fnShowFraction();
+		q = p++;
+		cout << "q : ";
+		q.fnShowFraction();
+		cout << "p : ";
+		p.fnShowFraction();
+
+		return 0;
+}
